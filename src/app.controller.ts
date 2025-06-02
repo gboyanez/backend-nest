d372a229219ecb1c1472c7c526db1b28e251ee0c
Gabriel <gabriel.yanez@agibiz.cl>
2025-06-02 22:55:38 +0000

    commit de frances

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('bonjour')
+  getHelloFrances(): string {
+  	return this.appService.getHelloFrances();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'hola mundo';
   }
+
+  getHelloFrances(): string {
+  	return 'frances';
+  }
 }
