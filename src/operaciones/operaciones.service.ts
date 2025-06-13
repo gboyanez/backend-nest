46944f3b34f04090aa368cdae0675f76d1e9bdf6
Gabriel <gabriel.yanez@agibiz.cl>
2025-06-13 17:17:47 +0000

    se agrega coverage

--- src/operaciones/operaciones.service.ts
+++ src/operaciones/operaciones.service.ts
@@ -6,11 +6,13 @@ export class OperacionesService {
     operar(operacion: string = '', a :number, b:number) {
 
         if(operacion === 'suma') {
-            return this.suma(a,b);
+            return this.#suma(a,b);
+        } else if (operacion === 'resta'){
+            return this.#resta(a,b);
         }
     }
 
-    suma(a: number, b: number){
+    #suma(a: number, b: number){
 
         if( a === undefined || b === undefined){
             throw new Error('No se puede llamar con numero indefinidos.');
@@ -20,6 +22,18 @@ export class OperacionesService {
             return NaN;
         }
         return a + b;
+    }
+
+    #resta(a: number, b: number){
+
+        if( a === undefined || b === undefined){
+            throw new Error('No se puede llamar con numero indefinidos.');
+        }
+
+        if( typeof a !== 'number' || typeof b !== 'number') {
+            return NaN;
+        }
+        return a - b;
     }
 
 }
